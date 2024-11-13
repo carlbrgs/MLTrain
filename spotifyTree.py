2ff3fc134c85853623af79ba9b235a54e35ad333
Carl Bourges <carlbrgs77@gmail.com>
2024-11-13 14:16:37 +0100

    Added accuracy

--- spotifyTree.py
+++ spotifyTree.py
@@ -25,3 +25,17 @@ print(clf)
 plt.figure(figsize=(12, 8))
 plot_tree(clf, feature_names=X.columns, class_names=Y.unique(), filled=True)
 plt.show()
+
+# ************* accuracy ************
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=42)
+
+clf.fit(X_train, Y_train)
+
+Y_pred = clf.predict(X_test)
+
+accuracy = accuracy_score(Y_test, Y_pred)
+print(f"Accuracy: {accuracy * 100:.2f}%")
